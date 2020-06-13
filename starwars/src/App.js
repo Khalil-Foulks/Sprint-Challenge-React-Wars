36fa59b171a5d2694494f5812f3eaca2d8bb4b0c
Khalil <khalilfoulks@gmail.com>
2020-06-13 16:10:20 -0400

    added useEffect

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import styled from 'styled-components'
+//import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +12,28 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [swData, setSwData] = useState([])
+  
+  useEffect(() =>{
+    axios.get('https://swapi.dev/api/people/')
+    .then(res =>{
+      //debugger
+      console.log(res)
+      setSwData(res.data.results)
+
+    })
+    .catch(err =>{
+      debugger
+      console.log('Error MSG:', err)
+    })
+  }, [])
+
+
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      
     </div>
   );
 }
